5ea076b445344063cfa0492a12202c7307e40d88
kunwar45 <knir45@gmail.com>
2019-07-13 13:48:34 -0400

    first comment

---
+++ spring-in-5-steps/src/main/java/com/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,14 @@
+package com.spring.basics.springin5steps;
+
+public class BinarySearchImpl {
+
+public int binarySearch(int[] numbers, int numberToSearchFor) {
+		
+		// implementing the Sorting Logic
+		
+		// Search the array
+		
+		return 3;
+	}
+
+}
---
+++ spring-in-5-steps/src/main/java/com/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -0,0 +1,17 @@
+package com.spring.basics.springin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn5StepsApplication {
+
+	public static void main(String[] args) {
+		
+		BinarySearchImpl binarySearch  = new BinarySearchImpl();
+		int resutl = binarySearch.binarySearch(new int[] {12,4, 6}, 3);
+		System.err.println(resutl);
+		SpringApplication.run(SpringIn5StepsApplication.class, args);
+	}
+
+}
